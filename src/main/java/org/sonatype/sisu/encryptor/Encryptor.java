9cfae4ff2129a3bd81a23afff782c3c17f6d834f
Jason Dillon <jason@planet57.com>
2010-11-04 18:02:22 -0700

    Port plexus-encryptor (http://svn.sonatype.org/spice/trunk/plexus-encryptor @r2914) over to sisu-injector.  Omitting cli bits for now.

---
+++ src/main/java/org/sonatype/sisu/encryptor/Encryptor.java
@@ -0,0 +1,46 @@
+package org.sonatype.sisu.encryptor;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+/**
+ * ???
+ *
+ * @since 1.0
+ */
+public interface Encryptor
+{
+
+    void generateKeys( File publicKey, File privateKey )
+        throws GeneralSecurityException, IOException;
+
+    void generateKeys( OutputStream publicKeyOut, OutputStream privateKeyOut )
+        throws GeneralSecurityException, IOException;
+
+    void encrypt( File source, File destination, File publicKey )
+        throws IOException, GeneralSecurityException;
+
+    void encrypt( InputStream plainInput, OutputStream encryptedOutput, InputStream publickKey )
+        throws IOException, GeneralSecurityException;
+
+    void encrypt( InputStream plainInput, OutputStream encryptedOutput, PublicKey key )
+        throws IOException, GeneralSecurityException;
+
+    void decrypt( File source, File destination, File privateKey )
+        throws IOException, GeneralSecurityException;
+
+    void decrypt( InputStream encryptedInput, OutputStream plainOutput, InputStream secretKey )
+        throws IOException, GeneralSecurityException;
+
+    void decrypt( InputStream encryptedInput, OutputStream plainOutput, PrivateKey key )
+        throws IOException, GeneralSecurityException;
+
+    void encrypt( File problemReportBundle, File encryptedZip, InputStream publicKey )
+        throws IOException, GeneralSecurityException;
+
+}
